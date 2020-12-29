a1b543572aca89bec4b03773e75b2884abc22594
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2020-12-29 16:34:04 +0600

    first commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <img src="./../public/logo192.png" alt=""/>
+            <p>Meme Generator 2k21</p>
+        </header>
+    )
+}
+
+export default Header
